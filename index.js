7a3a0fd88949aac03d8a92bd045f52dd900ed81d
Nenoeglah <134705870+Nenoeglah@users.noreply.github.com>
2023-06-16 14:59:10 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 =2;
+let multiply = num1 *num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 14;
+let num4 =5;
+let mod = num3 % num4;
+
+let numbers = [10, 5, 20, 15, 7];
+let max = Math.max.apply(null, numbers);
